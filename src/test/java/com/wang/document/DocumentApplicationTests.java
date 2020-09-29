bcb1a38337dfb950ca87ea1e19ac5260e6718d4b
王祥 <wangxiang@znlhzl.com>
2020-09-29 14:09:33 +0800

    Initial commit

---
+++ src/test/java/com/wang/document/DocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
